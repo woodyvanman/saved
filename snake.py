352caf9cd43c8de24d65bad74c77dee2c8338936
Shelton Rayner <162127621+woodyvanman@users.noreply.github.com>
2024-03-04 14:04:45 +0200

    Create snake.py

---
+++ snake.py
@@ -0,0 +1,26 @@
+ for item, _ in zip_longest(
+        soup.find_all(
+            "div",
+            attrs={"class": "s-result-item", "data-component-type": "s-search-result"},
+        ),
+        soup.find_all("div", attrs={"class": "a-row a-size-base a-color-base"}),
+    ):
+        try:
+            product_title = item.h2.text
+            product_link = "https://www.amazon.in/" + item.h2.a["href"]
+            product_price = item.find("span", attrs={"class": "a-offscreen"}).text
+            try:
+                product_rating = item.find("span", attrs={"class": "a-icon-alt"}).text
+            except AttributeError:
+                product_rating = "Not available"
+
+
+      def get_imdb_top_250_movies(url: str = "") -> dict[str, float]:
+    url = url or "https://www.imdb.com/chart/top/?ref_=nv_mv_250"
+    soup = BeautifulSoup(requests.get(url).text, "html.parser")
+    titles = soup.find_all("td", attrs="titleColumn")
+    ratings = soup.find_all("td", class_="ratingColumn imdbRating")
+    return {
+        title.a.text: float(rating.strong.text)
+        for title, rating in zip(titles, ratings)
+    }
